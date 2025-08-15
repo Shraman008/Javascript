c65ee739da43d1637803819285cc7f588de16cd3
Shraman Karmakar <karmakarshraman08@gmail.com>
2025-08-15 12:59:04 +0530

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) => we are using nodejs, not browser
+
+let name = "hitesh"
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+// object
+
+console.log(typeof "hitesh")
