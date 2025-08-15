c1df1ba8f2b4efb9b4487416cb69bc76ff4f9591
Shraman Karmakar <karmakarshraman08@gmail.com>
2025-08-15 12:58:40 +0530

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accoundId = 12245
+let accountEmail = "sman@gmail.com"
+var accountPassword = "12345"
+accountCity = 'Kolkata'
+
+console.log(accoundId);
+accountEmail = "sk@gmail.com"
+accountPassword = "32326"
+accountCity = "Chennai"
+let accountState
+
+console.log([accoundId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functions
+*/
